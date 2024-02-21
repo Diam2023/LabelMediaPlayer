6b9d6cffd4d37f498b72ae5aaff06a34e5e26ddb
monoliths <monoliths-uni@outlook.com>
2024-02-21 20:33:34 +0800

    Adjust some display setting

--- Source/AppDelegate.cc
+++ Source/AppDelegate.cc
@@ -1,20 +1,18 @@
 #include "AppDelegate.h"
 #include "MainScene.h"
 
-#define USE_AUDIO_ENGINE 1
-
-#if USE_AUDIO_ENGINE
-
-#    include "audio/AudioEngine.h"
-
-#endif
+#include "audio/AudioEngine.h"
 
 USING_NS_AX;
 
-static ax::Size designResolutionSize = ax::Size(720, 1280);
-static ax::Size smallResolutionSize = ax::Size(320, 480);
-static ax::Size mediumResolutionSize = ax::Size(768, 1024);
-static ax::Size largeResolutionSize = ax::Size(1536, 2048);
+
+// Use 16:9
+// Reference Link: https://zh.wikipedia.org/zh-cn/%E6%98%BE%E7%A4%BA%E5%88%86%E8%BE%A8%E7%8E%87%E5%88%97%E8%A1%A8
+// TODO Wait Increase colum pixel
+static ax::Size designResolutionSize = ax::Size(960, 540);
+static ax::Size smallResolutionSize = ax::Size(640, 360);
+static ax::Size mediumResolutionSize = ax::Size(1024, 768);
+static ax::Size largeResolutionSize = ax::Size(1920, 1080);
 
 AppDelegate::AppDelegate() {}
 
@@ -44,8 +42,8 @@ bool AppDelegate::applicationDidFinishLaunching() {
         director->setGLView(glView);
     }
 
-    // turn on display FPS
-    director->setStatsDisplay(true);
+    // FPS Display Option
+    director->setStatsDisplay(false);
 
     // set FPS. the default value is 1.0/60 if you don't call this
     director->setAnimationInterval(1.0f / 60);
@@ -58,19 +56,18 @@ bool AppDelegate::applicationDidFinishLaunching() {
     if (frameSize.height > mediumResolutionSize.height) {
         director->setContentScaleFactor(MIN(largeResolutionSize.height / designResolutionSize.height,
                                             largeResolutionSize.width / designResolutionSize.width));
-    }
+    } else if (frameSize.height > smallResolutionSize.height) {
         // if the frame's height is larger than the height of small size.
-    else if (frameSize.height > smallResolutionSize.height) {
         director->setContentScaleFactor(MIN(mediumResolutionSize.height / designResolutionSize.height,
                                             mediumResolutionSize.width / designResolutionSize.width));
-    }
+    } else {
         // if the frame's height is smaller than the height of medium size.
-    else {
         director->setContentScaleFactor(MIN(smallResolutionSize.height / designResolutionSize.height,
                                             smallResolutionSize.width / designResolutionSize.width));
     }
 
     // create a scene. it's an autorelease object
+    // First Scene Launched Here
     auto scene = utils::createInstance<MainScene>();
 
     // run
@@ -83,16 +80,12 @@ bool AppDelegate::applicationDidFinishLaunching() {
 void AppDelegate::applicationDidEnterBackground() {
     Director::getInstance()->stopAnimation();
 
-#if USE_AUDIO_ENGINE
     AudioEngine::pauseAll();
-#endif
 }
 
 // this function will be called when the app is active again
 void AppDelegate::applicationWillEnterForeground() {
     Director::getInstance()->startAnimation();
 
-#if USE_AUDIO_ENGINE
     AudioEngine::resumeAll();
-#endif
 }
