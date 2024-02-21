11ec988913d5cb9bcf0698ad5de796d13e9b0700
monoliths <monoliths-uni@outlook.com>
2024-02-21 20:32:21 +0800

    Added FileDialog

--- Source/FileDialog.cc
+++ Source/FileDialog.cc
@@ -3,3 +3,79 @@
 //
 
 #include "FileDialog.h"
+
+#if (AX_TARGET_PLATFORM == AX_PLATFORM_WIN32)
+
+#include <windows.h>
+#include <shobjidl.h>
+#include <iostream>
+
+bool FileDialog::init() {
+
+    // Create the FileOpenDialog object.
+    HRESULT hr = CoCreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_ALL,
+                                  IID_IFileOpenDialog, reinterpret_cast<void **>(&this->pFileOpenDialog));
+    return SUCCEEDED(hr);
+}
+
+bool FileDialog::show() {
+    // Show the Open dialog box.
+    HRESULT hr = this->pFileOpenDialog->Show(nullptr);
+
+    return SUCCEEDED(hr);
+}
+
+std::vector<std::string> &FileDialog::getSelectedResult() {
+    IShellItem *pItem = nullptr;
+    std::string result;
+    PWSTR pszFilePath = nullptr;
+
+    do {
+        HRESULT hr = this->pFileOpenDialog->GetResult(&pItem);
+        if (!SUCCEEDED(hr)) {
+            break;
+        }
+        hr = pItem->GetDisplayName(SIGDN_FILESYSPATH, &pszFilePath);
+        if (!SUCCEEDED(hr)) {
+            break;
+        }
+
+        int len = WideCharToMultiByte(CP_ACP, 0, pszFilePath, wcslen(pszFilePath), nullptr, 0, nullptr, nullptr);
+        if (len <= 0) {
+            break;
+        }
+        char *buffer = new char[len + 1];
+        if (buffer == nullptr) {
+            std::cout << "Error New Buffer" << std::endl;
+            break;
+        }
+        WideCharToMultiByte(CP_ACP, 0, pszFilePath, wcslen(pszFilePath), buffer, len + 1, nullptr, nullptr);
+
+        buffer[len] = '\0';
+        result.append(buffer);
+        delete[] buffer;
+    } while (false);
+
+    if (!result.empty()) {
+        this->selectedList.emplace_back(result);
+    }
+    if (pszFilePath != nullptr) {
+        CoTaskMemFree(pszFilePath);
+    }
+    if (pszFilePath != nullptr) {
+        pItem->Release();
+    }
+
+    return this->selectedList;
+}
+
+FileDialog &FileDialog::clear() {
+    selectedList.clear();
+    if (pFileOpenDialog != nullptr) {
+        pFileOpenDialog->Release();
+    }
+    pFileOpenDialog = nullptr;
+    return *this;
+}
+
+#endif
--- Source/FileDialog.h
+++ Source/FileDialog.h
@@ -1,16 +1,64 @@
 //
 // Created by 35691 on 2/21/2024.
 //
+#pragma once
 
-#ifndef LABELMEDIAPLAYER_FILEDIALOG_H
-#define LABELMEDIAPLAYER_FILEDIALOG_H
+#include <string>
+#include <vector>
+#include "axmol.h"
 
 
+#if (AX_TARGET_PLATFORM == AX_PLATFORM_WIN32)
+
+#include <windows.h>
+#include <shobjidl.h>
+
+#endif
 
 class FileDialog {
 
+private:
+
+#if (AX_TARGET_PLATFORM == AX_PLATFORM_WIN32)
+    IFileOpenDialog *pFileOpenDialog = nullptr;
+#endif
+
+protected:
+
+    /**
+     * The Selected Files
+     */
+    std::vector<std::string> selectedList;
+
+public:
+
+    bool init();
+
+    /**
+     * Show File Dialog
+     * @return Instance For Chain Call
+     */
+    bool show();
+
+    /**
+     * Wait Future Support
+     * @return thisInstance
+     */
+    FileDialog &setFilter();
+
+    FileDialog &multiSelect();
+
+    std::vector<std::string> &getSelectedResult();
+
+    FileDialog &clear();
+
+    /**
+     * @return Instance
+     */
+    static FileDialog &getInstance() {
+        static FileDialog fileDialog;
+        return fileDialog;
+    }
+
 };
 
-
-
-#endif //LABELMEDIAPLAYER_FILEDIALOG_H
